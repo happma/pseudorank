d61320eb0c22717bf3e9340ada77aecd78b11f25
Martin Happ <martinhapp@aon.at>
2018-06-10 14:22:26 +0200

    added the Hettmansperger-Norton test

--- R/S3method.R
+++ R/S3method.R
@@ -28,10 +28,45 @@ psrank.numeric <- function(data, group, ...){
   recursiveCalculation(data, group)
 }
 
-#' @method psrank formula
+#' @method psrank data.frame
 #' @rdname psrank
 #' @keywords export
-psrank.formula <- function(formula, data, ...){
+psrank.data.frame <- function(formula, data, ...){
   df <- model.frame(formula, data)
   recursiveCalculation(df[, 1], df[, 2])
 }
+
+
+#' Hettmansperger-Norton Trend Test for k-Samples
+#'
+#' @description This function calculates the Hettmansperger-Norton trend test using pseudo-ranks under the null hypothesis H0F: F_1 = ... F_k = 0.
+#' @rdname hettmansperger_norton_test
+#' @param data numeric vector containing the data or a data.frame
+#' @param group ordered factor vector for the groups
+#' @param alternative either decreasing or increasing
+#' @param formula formula object
+#' @param ... further arguments are ignored
+#' @return Returns an object.
+#' @example R/example_2.txt
+#' @references Brunner, E., Bathke A. C. and Konietschke, F. Rank- and Pseudo-Rank Procedures in Factorial Designs - Using R and SAS. Springer Verlag. to appear.
+#' @references Hettmansperger, T. P., & Norton, R. M. (1987). Tests for patterned alternatives in k-sample problems. Journal of the American Statistical Association, 82(397), 292-299
+#' @keywords export
+hettmansperger_norton_test <- function(data, ...) {
+  UseMethod("hettmansperger_norton_test")
+}  
+
+#' @method hettmansperger_norton_test numeric
+#' @rdname hettmansperger_norton_test
+#' @keywords export
+hettmansperger_norton_test.numeric <- function(data, group, alternative = c("decreasing", "increasing"), ...) {
+  return(hettmansperger_norton_test_internal(data, group, alternative = alternative, formula = NULL, ...))
+}  
+
+#' @method hettmansperger_norton_test data.frame
+#' @rdname hettmansperger_norton_test
+#' @keywords export
+hettmansperger_norton_test.data.frame <- function(formula, data, alternative = c("decreasing", "increasing"), ...) {
+  model <- model.frame(formula, data = data)
+  colnames(model) <- c("data", "group")
+  return(hettmansperger_norton_test_internal(model$data, model$group, alternative = alternative, formula = formula, ...))
+}  
---
+++ R/hettmansperger.R
@@ -0,0 +1,101 @@
+#' @keywords internal
+I <- function(d) {
+  return(diag(d))
+}
+
+#' @keywords internal
+J <- function(d) {
+  return(rep(1,d)%*%t(rep(1,d)))
+}
+
+
+#' Hettmansperger-Norton Trend Test for k-Samples
+#'
+#' @description This function calculates the Hettmansperger-Norton trend test using pseudo-ranks under the null hypothesis H0F: F_1 = ... F_k = 0.
+#' @param data numeric vector containing the data
+#' @param group ordered factor vector for the groups
+#' @param alternative either decreasing or increasing
+#' @param formula formula object
+#' @param ... further arguments are ignored
+#' @return Returns a data.frame with the results
+#' @example R/example_2.txt
+#' @references Brunner, E., Bathke A. C. and Konietschke, F. Rank- and Pseudo-Rank Procedures in Factorial Designs - Using R and SAS. Springer Verlag. to appear.
+#' @references Hettmansperger, T. P., & Norton, R. M. (1987). Tests for patterned alternatives in k-sample problems. Journal of the American Statistical Association, 82(397), 292-299
+#' @keywords internal
+hettmansperger_norton_test_internal <- function(data, group, alternative = c("decreasing", "increasing"), formula = NULL, ...) {
+  
+  stopifnot(is.numeric(data), is.factor(group), is.ordered(group))
+  
+  n <- as.numeric(as.matrix(table(group)))
+  a <- length(n)
+  df <- data.frame(pranks = psrank.numeric(data, group), group = group)
+  df <- df[order(df$group),]
+  pHat <- 1/sum(n)*(summaryBy(pranks~group,data=df, FUN = mean)[, 2]-1/2)
+  n <- summaryBy(pranks~group,data=df, FUN = length)[, 2]
+  alternative <- match.arg(alternative)
+  w <- rep(1, a)
+  switch(alternative, 
+         decreasing={
+           w <- a:1
+         },
+         increasing={
+           w <- 1:a
+         }
+  )
+  
+  W <- diag(n)%*%(I(a) - 1/sum(n)*J(a)%*%diag(n))
+  v2 <- 1/sum(n)^2*1/(sum(n)-1)*sum( (df$pranks - (sum(n)+1)/2 )^2 )
+  sigmaHat2 <- sum(n)*v2*t(w)%*%W%*%diag(1/n)%*%W%*%w
+  
+  test <- sqrt(sum(n))*t(w)%*%W%*%pHat*1/sqrt(sigmaHat2)
+  
+  pValue <- 1 - pnorm(test)
+  
+  output <- list()
+  output$test <- test
+  output$pValue <- pValue
+  output$ss <- n
+  output$pHat <- pHat
+  output$alternative <- alternative
+  output$formula <- formula
+  class(output) <- "pseudorank"
+  
+  return(output)
+  
+}
+
+#' @keywords export
+print.pseudorank <- function(x, ...) {
+  cat("Hettmansperger-Norton Trend Test")
+  cat("\n","\n")
+  if(!is.null(x$formula)) {
+    cat("Call:", "\n")
+    print(x$formula)
+    cat("\n")
+  }
+  cat("Alternative: ", x$alternative, "\n")
+  cat("Test Statistic: ", x$test, "\n")
+  cat("p-Value: ", x$pValue, "\n")
+  cat("\n")
+  cat("Descriptive:\n")
+  df <- data.frame(n = x$ss, p = x$pHat)
+  print(df, row.names = FALSE)
+}
+
+#' @keywords export
+summary.pseudorank <- function(object, ...) {
+  cat("Hettmansperger-Norton Trend Test")
+  cat("\n", "\n")
+  if(!is.null(object$formula)) {
+    cat("Call:", "\n")
+    print(object$formula)
+    cat("\n")
+  }
+  cat("Alternative: ", object$alternative, "\n")
+  cat("Test Statistic: ", object$test, "\n")
+  cat("p-Value: ", object$pValue, "\n")
+  cat("\n")
+  cat("Descriptive:\n")
+  df <- data.frame(n = object$ss, p = object$pHat)
+  print(df, row.names = FALSE)
+}

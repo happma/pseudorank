ca3177c627c5d669b4abba57c30b5ea8a50f9e76
Happ Martin <b1011921@is.sbg.ac.at>
2018-08-08 13:12:53 +0200

    unit test for hettmansperger test with ranks added

--- tests/testthat/test_hettmansperger.R
+++ tests/testthat/test_hettmansperger.R
@@ -44,3 +44,15 @@ test_that("function hettmansperger_norton_test missing values", {
   expect_error(pseudorank::hettmansperger_norton_test(df2$data, df2$group, alternative = "increasing"))
   expect_error(pseudorank::hettmansperger_norton_test(data~group, data = df2, alternative = "increasing"))
 })
+
+
+true_result <- 1.338493
+test_df <- data.frame(data = c(1,7,1,2,3,3,5.5,6,7), group = c(1,1,1,2,2,3,3,3,3))
+test_df$group <- factor(test_df$group, ordered = TRUE)
+
+test_that("function hettmansperger_norton_test with ranks", {
+  expect_equivalent(pseudorank::hettmansperger_norton_test(test_df$data, test_df$group, alternative = "increasing", na.rm=TRUE, pseudoranks = FALSE)$test, true_result, tolerance = 1e-04)
+  expect_equivalent(pseudorank::hettmansperger_norton_test(data~group, data = test_df, alternative = "increasing", na.rm=TRUE, pseudoranks = FALSE)$test, true_result, tolerance = 1e-04)
+  expect_equivalent(pseudorank::hettmansperger_norton_test(test_df$data, test_df$group, alternative = "decreasing", na.rm=TRUE, pseudoranks = FALSE)$test, -true_result, tolerance = 1e-04)
+  expect_equivalent(pseudorank::hettmansperger_norton_test(data~group, data = test_df, alternative = "decreasing", na.rm=TRUE, pseudoranks = FALSE)$test, -true_result, tolerance = 1e-04)
+})
